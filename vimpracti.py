79e9e6eb8bfa399e6014a29f8a2f0880587bf83b
Matus Luknar <matus.luknar@websupport.sk>
2015-08-05 18:17:28 +0200

    added more shortcuts

--- vimpracti.py
+++ vimpracti.py
@@ -6,17 +6,95 @@ import sys, tty, termios
 
 QUESTION_VAULT = [
     {
-        'k': 'Move cursor one character top',
+        ## navigation
+        'h': 'Move cursor one character left',
         'j': 'Move cursor one character down',
+        'k': 'Move cursor one character up',
         'l': 'Move cursor one character right',
-        'h': 'Move cursor one character left',
-        'w': 'Move one word',
-        'e': 'Move to the end of the word',
-        'b': 'Move to the beginning of the word',
-        'h': 'Move cursor one character left',
-        'h': 'Move cursor one character left',
-        'h': 'Move cursor one character left',
-        'h': 'Move cursor one character left',
+        'w': 'Jump by start of words (punctuation considered words)',
+        'W': 'Jump by words (spaces separate words)',
+        'e': 'jump to end of words (punctuation considered words)',
+        'E': 'jump to end of words (no punctuation)',
+        'b': 'jump backward by words (punctuation considered words)',
+        'B': 'jump backward by words (no punctuation)',
+        '0': 'start of line',
+        '^': 'first non-blank character of line',
+        '$': 'end of line',
+        'G': 'Go To command (prefix with number)',
+        'i': 'start insert mode at cursor',
+        'I': 'insert at the beginning of the line',
+        'a': 'append after the cursor',
+        'A': 'append at the end of the line',
+        'o': 'open (append) blank line below current line (no need to press return)',
+        'O': 'open blank line above current line',
+        'ea': 'append at end of word',
+        'Esc': 'exit insert mode',
+
+        ## editing
+        'r': 'replace a single character (does not use insert mode)',
+        'J': 'join line below to the current one',
+        'cc': 'change (replace) an entire line',
+        'cw': 'change (replace) to the end of word',
+        'c$': 'change (replace) to the end of line',
+        's': 'delete character at cursor and subsitute text',
+        'S': 'delete line at cursor and substitute text (same as cc)',
+        'xp': 'transpose two letters (delete and paste, technically)',
+        'u': 'undo',
+        '.': 'repeat last command',
+
+        ## marking text (in visual mode)
+# shortcuts are repeated from different modes,
+# therefore this section is commented out otherwise it
+# might cause problems
+        # 'v': 'start visual mode, mark lines, then do command (such as y-yank)',
+        # 'V': 'start Linewise visual mode',
+        # 'o': 'move to other end of marked area',
+        # 'O': 'move to Other corner of',
+        # 'aw': 'mark a word',
+        # 'ab': 'a () block (with braces)',
+        # 'aB': 'a {} block (with brackets)',
+        # 'ib': 'inner () block',
+        # 'iB': 'inner {} block',
+        # 'Esc': 'exit visual mode',
+
+
+        ## visual commands
+        '>': 'shift right',
+        '<': 'shift left',
+        'y': 'yank (copy) marked text',
+        'd': 'delete marked text',
+        '~': 'switch case',
+
+
+        ## cut and paste
+        'yy': 'yank (copy) a line',
+        '2yy': 'yank 2 lines',
+        'yw': 'yank word',
+        'y$': 'yank to end of line',
+        'p': 'put (paste) the clipboard after cursor',
+        'P': 'put (paste) before cursor',
+        'dd': 'delete (cut) a line',
+        'dw': 'delete (cut) the current word',
+        'x': 'delete (cut) current character',
+        
+        ## exiting
+        ':w': 'write (save) the file, but don\'t exit',
+        ':wq': 'write (save) and quit',
+        ':q': 'quit (fails if anything has changed)',
+        ':q!': 'quit and throw away changes',
+
+        # search/replace
+        '/pattern': 'search for pattern',
+        '?pattern': 'search backward for pattern',
+        'n': 'repeat search in same direction',
+        'N': 'repeat search in opposite direction',
+
+        # working with multiple files
+        ':e filename': 'Edit a file in a new buffer',
+        ':bn': 'go to next buffer',
+        ':bp': 'go to previous buffer',
+        ':bd': 'delete a buffer (close a file)',
+        ':sp filename': 'Open a file in a new buffer and split window'
     },
 ]
 
